b2119f4b02b8ba20f51dca9f661eae805f40297c
Nathan Martin <na.martin@ufl.edu>
2021-03-03 20:36:15 -0500

    fighter foundation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pyre_finder',
+      version='0.0.1',
+      description='Realtime fire monitoring, deployment, and management solution.',
+      package_dir={'', 'src'},
+      license='MIT',
+      author='PyreWatch')
